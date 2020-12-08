c658d8825f39c719d4e5d70d1cf6a63391e16e62
Simonestevo <simonestevenson@gmail.com>
2020-12-08 14:40:43 +1100

    Added in beth's code to make 3d plots and dendograms

--- analyse_indicators.R
+++ analyse_indicators.R
@@ -1,6 +1,9 @@
 
 # Using R version 4.0.3
 
+## Clear the space
+rm(list = ls()) # clear memory
+
 # Load packages ----
 
 # install.packages("tidyverse", dependencies = TRUE)
@@ -17,25 +20,45 @@
 # install.packages("tm", dependencies = TRUE)
 # install.packages("PerformanceAnalytics", dependencies = TRUE)
 
+
+# Data handling and table reshaping
 library(tidyverse)
+library(reshape2)
+library(devtools)
+library(data.table)
+library(rlist)
+library(e1071)
+library(psych)
+
+# Plotting
 library(ggplot2)
-#library(grid)
+library(RColorBrewer)
+library(ggbiplot)
+library(plot3D)
+library(plotly)
 library(viridis)
 library(png)
 library(gridExtra)
-library(reshape2)
 library(GGally)
-library(mapview)
-library(psych) #not loaded
-library(e1071)
-library(tm) #notloaded
-library(PerformanceAnalytics) #not loaded
-library(data.table)
-library(rlist)
-library(factoextra)
 library(ggcorrplot)
-library(MASS)
+
+# Maps
 library(sf)
+
+# PCA and Clustering
+library(factoextra)
+library(FactoMineR)
+library(corrplot)
+library(ape)
+library(MASS)
+
+
+# library(mapview)
+# library(psych) #not loaded
+# library(tm) #notloaded
+# library(PerformanceAnalytics) #not loaded
+# library(grid)
+
 
 # Set input and output locations ----
 
@@ -394,8 +417,242 @@ write.csv(pca_loadings, file.path(current_analysis_outputs, "pca_loadings.csv"))
 var <- get_pca_var(pca)
 variable_contributions <- var$contrib
 
-write.csv(variable_contributions, file.path(current_analysis_outputs, "pca_variable_contributions.csv"))
+write.csv(variable_contributions, file.path(current_analysis_outputs, 
+                                            "pca_variable_contributions.csv"))
 
+##Using Beth's code ----
+
+pl.data <- pca_data_5[,c(22:28)]
+rownames(pl.data) <- pca_data_5[,1]
+
+dimC <- dim(pl.data)
+
+pl.data <- scale(pl.data[, 2:dimC[2]])
+
+names(pl.data) <- colnames(pl.data)
+
+pl.data <- as.data.frame(pl.data)
+
+pc.f <- formula(paste("~", paste(names(pl.data)[1:dimC[2] - 1], collapse = "+")))
+pl.pca <- princomp(pc.f, cor=TRUE, data=pl.data)
+
+
+# Print out PCA loadings
+pl.pca$loadings
+
+# Print out eigenvalues
+pl.pca$sd^2
+
+# Print PCA summary
+summary(pl.pca)
+
+# Plot results - look to see number of PCA axes to retain
+plot(pl.pca, type="lines")
+
+### Shows 3 axes likely sufficient
+# Plot points
+text(pl.pca$scores, labels=as.character(row.names(pl.data)), pos=1, cex=0.7)
+
+# Biplot of PCA
+biplot(pl.pca, cex=0.8, col=c(1,8))
+
+# Linked biplot
+View(pl.pca$scores)
+library(ggplot2)
+df <- data.frame(comp1=pl.pca$scores[,1],
+                 comp2=pl.pca$scores[,2])
+ggplot(data = df, aes(x=comp1, y=comp2, group=1)) +
+  geom_point(size=5, aes(colour=rownames(pl.pca$scores))) +
+  geom_path(size = 0.2) +
+  geom_text(label=rownames(pl.pca$scores)) + 
+  theme(legend.position="none")
+
+# Alt approach
+
+PoV <- pl.pca$sdev^2/sum(pl.pca$sdev^2)
+fviz_eig(pl.pca)
+
+# Put on row named
+#Rename Col 1
+View(pl.data)
+
+pcx <- pl.pca$scores[,1]
+pcy <- pl.pca$scores[,2]
+pcz <- pl.pca$scores[,3]
+
+pcxlab <- paste("PC1 (", round(PoV[1] * 100, 2), "%)")
+pcylab <- paste("PC2 (", round(PoV[2] * 100, 2), "%)")
+pczlab <- paste("PC3 (", round(PoV[3] * 100, 2), "%)")
+
+View(pl.pca$scores)
+
+# 3D as points
+scatter3D(pcx, pcy, pcz, bty = "g", pch = 20, cex = 2, 
+          col = gg.col(100), theta = 150, phi = 0, main = "PCA Scores", xlab = pcxlab,
+          ylab =pcylab, zlab = pczlab)
+text3D(pcx, pcy, pcz,  labels = rownames(pl.pca$scores), add = TRUE, colkey = FALSE, cex = 0.7)
+
+# 3D as connected line
+scatter3D(pcx, pcy, pcz, bty = "g", type = "b", pch = 20, cex = 2, 
+          col = gg.col(100), theta = 150, phi = 0, lwd = 4, main = "PCA Scores", xlab = pcxlab,
+          ylab =pcylab, zlab = pczlab)
+text3D(pcx, pcy, pcz,  labels = rownames(pl.pca$scores), add = TRUE, colkey = FALSE, cex = 0.7)
+
+# Plot3D with plotly
+#manual_palette <- c("#551A8B", "#8968CD", "#AB82FF", "#FF8C00", "#CD6600", "#8B4500")
+
+df3D <- data.frame(comp1=pl.pca$scores[,1],
+                   comp2=pl.pca$scores[,2],
+                   comp3=pl.pca$scores[,3])
+fig <- plot_ly(df3D, x = ~comp1, y = ~comp2, z = ~comp3, color = ~comp3,  mode = 'lines+markers',
+               # Hover text:
+               text = ~rownames(pl.pca$scores))
+fig <- fig %>% add_markers()
+fig <- fig %>% add_text(textposition = "top right")
+fig <- fig %>% layout(scene = list(xaxis = list(title = pcxlab),
+                                   yaxis = list(title = pcylab),
+                                   zaxis = list(title = pczlab)),
+                      annotations = list(
+                        x = 1.13,
+                        y = 1.05,
+                        text = 'PC3 Score',
+                        showarrow = FALSE
+                      ))
+fig
+
+###Use prcomp() instead - this uses singular value decomposition 
+#pl.data <- read.table('Yr_as_col_new_78_spp_India_data.csv',sep=",",header=T,row.names=1)
+
+res.pca <- prcomp(pl.data, scale = TRUE)
+res.pca$x
+
+# Visualize eigenvalues (scree plot). Show the percentage of variances explained by each principal component.
+fviz_eig(res.pca)
+
+#Graph of individuals. Individuals with a similar profile are grouped together.
+fviz_pca_ind(res.pca,
+             col.ind = "contrib", # Color by congtribution
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE,     # Avoid text overlapping
+             #label=SP_as_col$Year
+) +
+  labs(title ="PCA", x = "PC1", y = "PC2")
+
+# Graph of variables
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#36648B", "#FFA500", "#8B2500"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+# Compute hierarchical clustering on principal components
+res.pca4 <- PCA(pl.data, ncp = 3, graph = FALSE)
+res.hcpc <- HCPC(res.pca4, graph = FALSE)
+
+fviz_dend(res.hcpc, 
+          cex = 0.7,                     # Label size
+          palette = "jco",               # Color palette see ?ggpubr::ggpar
+          rect = TRUE, rect_fill = TRUE, # Add rectangle around groups
+          rect_border = "jco",           # Rectangle color
+          labels_track_height = 0.8      # Augment the room for labels
+)
+
+fviz_cluster(res.hcpc,
+             repel = TRUE,            # Avoid label overlapping
+             show.clust.cent = TRUE, # Show cluster centers
+             palette = "jco",         # Color palette see ?ggpubr::ggpar
+             ggtheme = theme_bw(),
+             main = "Factor map"
+)
+
+
+PoV <- res.pca$sdev^2/sum(res.pca$sdev^2)
+fviz_eig(res.pca)
+
+
+pcx <- res.pca$x[,1]
+pcy <- res.pca$x[,2]
+pcz <- res.pca$x[,3]
+
+pcxlab <- paste("PC1 (", round(PoV[1] * 100, 2), "%)")
+pcylab <- paste("PC2 (", round(PoV[2] * 100, 2), "%)")
+pczlab <- paste("PC3 (", round(PoV[3] * 100, 2), "%)")
+
+# 3D as points
+scatter3D(pcx, pcy, pcz, bty = "g", pch = 20, cex = 2, 
+          col = gg.col(100), theta = 150, phi = 0, main = "PCA Scores", xlab = pcxlab,
+          ylab =pcylab, zlab = pczlab)
+text3D(pcx, pcy, pcz,  labels = rownames(res.pca$x), add = TRUE, colkey = FALSE, cex = 0.7)
+
+# 3D as connected line
+scatter3D(pcx, pcy, pcz, bty = "g", type = "b", pch = 20, cex = 2, 
+          col = gg.col(100), theta = 120, phi = 0, lwd = 2, main = "PCA Scores", xlab = pcxlab,
+          ylab =pcylab, zlab = pczlab)
+text3D(pcx, pcy, pcz,  labels = rownames(res.pca$x), add = TRUE, colkey = FALSE, cex = 0.7)
+
+# Rainbow version
+df <- data.frame(comp1=pcx, comp2=pcy)
+ggplot(data = df, aes(x=comp1, y=comp2, group=1)) +
+  geom_point(size=5, aes(colour=rownames(res.pca$x))) +
+  geom_path(size = 0.2) +
+  geom_text(label=rownames(res.pca$x)) + 
+  theme(legend.position="none")
+
+ggplot(data = df, aes(x=comp1, y=comp2, group=1)) +
+  geom_point(size=5, aes(colour=rownames(res.pca$x))) +
+  geom_text(label=rownames(res.pca$x)) + 
+  theme(legend.position="none")
+
+#Plotly version
+df3D <- data.frame(comp1=pcx, comp2=pcy, comp3=pcz)
+fig <- plot_ly(df3D, x = ~comp1, y = ~comp2, z = ~comp3, color = ~comp3,  mode = 'lines+markers',
+               # Hover text:
+               text = ~rownames(res.pca$x))
+fig <- fig %>% add_markers()
+fig <- fig %>% add_text(textposition = "top right")
+fig <- fig %>% layout(scene = list(xaxis = list(title = pcxlab),
+                                   yaxis = list(title = pcylab),
+                                   zaxis = list(title = pczlab)),
+                      annotations = list(
+                        x = 1.13,
+                        y = 1.05,
+                        text = 'PC3 Score',
+                        showarrow = FALSE
+                      ))
+fig
+
+################################# DENDROGRAMS on RAW DATA ###############################
+# Dendrogram flowing down the page
+dd <- dist(scale(pl.data), method = "euclidean")
+hc <- hclust(dd, method = "ward.D2")
+plot(hc, hang = -1, cex = 0.6)   # Put the labels at the same height: hang = -1
+plot(hc, hang = -1, cex = 1.5)   # Put the labels at the same height: hang = -1
+
+# Dendrogram flowing across the page
+# Define nodePar
+hcd <- as.dendrogram(hc)
+nodePar <- list(lab.cex = 0.6, pch = c(NA, 19),  cex = 0.7, col = "blue")
+par(cex=0.5)   # Customized plot; remove labels
+par(cex=1.0)   # Customized plot; remove labels
+plot(hcd,  xlab = "Height", nodePar = nodePar, horiz = TRUE)
+
+# Unrooted - dendrogram on an arc
+plot(as.phylo(hc), type = "unrooted", cex = 0.6, no.margin = TRUE)
+
+# Fan
+par(cex=0.8)
+par(cex=1.0)
+plot(as.phylo(hc), type = "fan")
+colors = c("red", "blue", "green", "black", "yellow", "purple", "grey", "brown", "magenta", "cyan", "violetred", "tomato")
+clus = cutree(hc, 12)
+plot(as.phylo(hc), type = "fan", tip.color = colors[clus],label.offset = 1, cex = 0.7)
+plot(as.phylo(hc), type = "fan", tip.color = colors[clus],label.offset = 1, cex = 1.0)
+
+
+
+
+##################################################################
+# Back to my code
 # look at the eigen values and cumulative variance plot
 
 tiff(file = file.path(current_analysis_outputs, "pca_screeplot.tiff"), 

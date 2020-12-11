68a0e6b5ac99b97d537219fcba9139f140b2bfe5
Simonestevo <simonestevenson@gmail.com>
2020-12-11 14:34:53 +1100

    Added tsn to threat data so they could be joined more accurately to the species data

--- calculate_indicators.R
+++ calculate_indicators.R
@@ -345,6 +345,150 @@ get_lpi <- function(data, name) {
   
 }
 
+get_binomial_list <- function(data) {
+  
+  data$binomial <- as.character(data$binomial)
+  
+  data <- unname(unlist(data$binomial))
+  
+  data <- data[!is.na(data)]
+  
+  binomial_list <- unique(data)
+  
+}
+
+find_synonyms <- function( species ) {
+  
+  require(taxizedb)
+  
+  # Get taxonomic information 
+  
+  sql_integer_list <- function(x){
+    
+    if (any(is.na(x))) {
+      stop("Cannot pass NA into SQL query")
+    }
+    x <- as.character(x)
+    if (!all(grepl('^[0-9]+$', x, perl = TRUE))) {
+      stop("Found non-integer where integer required in SQL input")
+    }
+    paste(x, collapse = ", ")
+  }
+  
+  taxizedb::db_download_ncbi()
+  
+  src_ncbi <- taxizedb::src_ncbi()
+  
+  species <- unique( species )
+  
+  # create the empty final results dataframe
+  
+  results <- data.frame(
+    species = species,
+    found = rep( FALSE, length( species ) ),
+    tsn = rep( NA, length( species ) ),
+    accepted_name = rep( NA, length( species) ),
+    synonyms = rep( NA, length( species) ),
+    common_name = rep( NA, length( species ) ),
+    kingdom = rep( NA, length( species) ),
+    phylum = rep( NA, length( species) ),
+    class = rep( NA, length( species) ),
+    order = rep( NA, length( species) ),
+    family = rep( NA, length( species) ),
+    genus = rep( NA, length( species) ),
+    stringsAsFactors = FALSE
+  )
+  
+  # Get the taxonomic IDs 
+  
+  # get NCBI IDs where available (NA if not available)
+  
+  species <- as.data.frame(species)
+  species$species <- as.character((species$species))
+  species_taxid <- sapply( species, taxizedb::name2taxid, out_type = "summary" )
+  species_taxid <- as.data.frame(do.call(cbind, species_taxid))
+  colnames(species_taxid) <- c("species", "tsn")
+  
+  # Add tsn ID to results dataframe
+  
+  results <- results %>%
+    dplyr::select(-tsn) %>%
+    merge(species_taxid, by = "species", all = TRUE) %>%
+    dplyr::select(species, found, tsn, everything())
+  
+  # load accepted names into the results dataframe
+  
+  for( tsn in na.omit( unique( results$tsn ) ) ) {
+    
+    classification <- taxizedb::classification( tsn )[[tsn]]
+    relevant_rows <- which( results$tsn == tsn )
+    
+    # set taxonomic information
+    if( 
+      is.data.frame( classification )
+      && length( relevant_rows ) != 0
+      && length(which(classification$rank == "kingdom")) != 0
+      && length(which(classification$rank == "phylum")) != 0
+      && length(which(classification$rank == "class")) != 0
+      && length(which(classification$rank == "order")) != 0
+      && length(which(classification$rank == "family")) != 0
+      && length(which(classification$rank == "genus")) != 0
+    ) {
+      results[relevant_rows, "kingdom" ] <- classification[[which(classification$rank == "kingdom"), "name"]]
+      results[relevant_rows, "phylum" ] <- classification[[which(classification$rank == "phylum"), "name"]]
+      results[relevant_rows, "class" ] <- classification[[which(classification$rank == "class"), "name"]]
+      results[relevant_rows, "order" ] <- classification[[which(classification$rank == "order"), "name"]]
+      results[relevant_rows, "family" ] <- classification[[which(classification$rank == "family"), "name"]]
+      results[relevant_rows, "genus" ] <- classification[[which(classification$rank == "genus"), "name"]]
+    }
+    
+    # set accepted name
+    if( 
+      is.data.frame( classification )
+      && length( relevant_rows ) != 0
+      && length(which(classification$rank == "species")) != 0
+    ) {
+      results[relevant_rows, "accepted_name" ] <- classification[[which(classification$rank == "species"), "name"]]
+      results[relevant_rows, "found" ] <- TRUE
+    }
+    
+    # set common name
+    common_names <- taxizedb::sql_collect(src_ncbi, paste0("SELECT * FROM names WHERE tax_id=", tsn, " AND name_class='common name'" ) )
+    results[which(results$tsn == tsn), "common_name" ] <- paste0( common_names$name_txt, collapse = ", " )
+    
+    # set synonyms
+    common_names <- taxizedb::sql_collect(src_ncbi, paste0("SELECT * FROM names WHERE tax_id=", tsn, " AND name_class='common name'" ) )
+    results[which(results$tsn == tsn), "common_name" ] <- paste0( common_names$name_txt, collapse = ", " )
+  }
+  
+  # Find synonyms
+  
+  # get the list of relevant ids
+  relevant_tsns <- na.omit( unique( results$tsn ) )
+  
+  # make a list of all the names available and tsns to match
+  query <- "SELECT * FROM names WHERE tax_id IN(%s) AND( name_class='scientific name' OR name_class='synonym')"
+  query <- sprintf(query, sql_integer_list( relevant_tsns ))
+  search_names <- taxizedb::sql_collect(src_ncbi, query)
+  
+  synonyms <- data.frame(
+    tsn = search_names$tax_id,
+    binomial = search_names$name_txt
+  )
+  
+  synonyms$binomial <- as.character(synonyms$binomial)
+  
+  synonyms <- synonyms %>%
+    merge(results, by = "tsn") %>%
+    dplyr::select(-species) %>%
+    dplyr::select(binomial, tsn, everything())
+  
+  # return( synonyms )
+  return( synonyms )
+  
+}
+
+
 # Get ecoregions and their attributes ----
 
 if (("Ecoregions2017Valid.rds" %in% list.files(file.path(inputs, 
@@ -995,33 +1139,64 @@ saveRDS(rli_all_2005_values, file.path(indicator_outputs,
 # Split the species by their major threat
 
 threat_files <-  list.files(interim_outputs, recursive = FALSE)[grepl("threat_data.rds",
-                                                                      list.files(interim_outputs))]
+                                              list.files(interim_outputs))]
+
 threat_list <- lapply(file.path(interim_outputs, threat_files), readRDS)
+
 all_threat_data <- do.call(rbind, threat_list)
 
-threat_scheme <- read.csv(file.path(inputs, "iucn_threats\\iucn_threat_classification_scheme.csv"))
+# Check the threat data for synonyms and add tsn so we can match with 
+# species_data more accurately
+
+threat_binomials <- get_binomial_list(all_threat_data)
+
+system.time(threat_data_synonyms <- find_synonyms(threat_binomials))
+
+saveRDS(threat_data_synonyms, file.path(indicator_outputs, 
+                                        paste(location, eco_version, 
+                                              "threat_data_synonyms.rds",
+                                              sep = "_")))
+
+# Join the tsn to the threat data
+
+all_threat_data <- all_threat_data %>%
+                   merge(threat_data_synonyms[c("tsn", "binomial", 
+                                                   "accepted_name")],
+                      by = "binomial") %>%
+                   distinct(.) %>%
+                   dplyr::select(-binomial) %>%
+                   rename(binomial = accepted_name)
+
+# Get the threat scheme info
+
+threat_scheme <- read.csv(file.path(inputs, 
+                      "iucn_threats\\iucn_threat_classification_scheme.csv"))
+
 threat_scheme$code <- as.character(threat_scheme$code)
+
+# Join the threat data to the species data
 
 species_threat_data <- all_threat_data %>%
                        merge(threat_scheme[c("code", "headline", 
                                             "headline_name", 
                                             "included_in_hfp")], # Merge with the threat scheme
                             by = "code") %>%
-                       filter(scope == "Majority (50-90%)" | scope == "Whole (>90%)") %>% # only use data when we know it impacts most of the spp range, otherwise you can't assume it is present in all ecoregions the spp is in
+                       filter(scope == "Majority (50-90%)" | 
+                              scope == "Whole (>90%)") %>% # only use data when we know it impacts most of the spp range, otherwise you can't assume it is present in all ecoregions the spp is in
                        distinct(.) %>%
-                       merge(species_data[c("binomial", "tsn", 
+                       merge(species_data[c("tsn", 
                                             "ecoregion_id")], 
-                             by = "binomial") %>%
+                             by = "tsn") %>%
                        dplyr::select(-code, - invasive) %>%
                        dplyr::select(ecoregion_id, binomial, tsn, headline,
                                     headline_name, title, included_in_hfp) %>%
                        distinct(.) %>%
                        mutate(included_in_hfp = ifelse(included_in_hfp == "1, 0",
                                                       "1",
-                                                      ifelse(included_in_hfp == "0, 1",
-                                                             "1", included_in_hfp)))
+                                                ifelse(included_in_hfp == "0, 1",
+                                                      "1", included_in_hfp)))
 
-# TODO: IMPORTANT - CHECK TSNS? ----
+# Subset species affected by land use and not affected by land use
 
 species_affected_by_landuse <- species_threat_data %>%
                                filter(included_in_hfp == "1") %>%
@@ -1170,17 +1345,6 @@ saveRDS(rli_other_2005_values, file.path(indicator_outputs,
                                               "RLI-other_all_classes.rds",
                                               sep = "_")))
 }
-
-## Check - compare all spp with land use spp RLIs
-
-rli_test <- rli_all_2005_values %>%
-            rename(RLI = raw_indicator_value) %>%
-            merge(threat_rli_all_2005_values[,c("ecoregion_id", 
-                                             "raw_indicator_value")], 
-                  by = "ecoregion_id") %>%
-            rename(RLI_LU = raw_indicator_value) %>%
-            mutate(diff = RLI - RLI_LU)
-
 
 
 # Human Footprint Index  ----
@@ -2168,10 +2332,47 @@ ecoregion_hfp_threats <- readRDS(file.path(indicator_outputs,
   
 } else {
 
-threat_files <-  list.files(interim_outputs, recursive = FALSE)[grepl("threat_data.rds",list.files(interim_outputs))]
-threat_list <- lapply(file.path(interim_outputs, threat_files), readRDS)
-all_threat_data <- do.call(rbind, threat_list)
+  # Check if threat data has already been loaded and tsn added, otherwise do 
+  # this first
+  
+if(is.null(all_threat_data) == TRUE) {
+  
+  threat_files <-  list.files(interim_outputs, 
+                              recursive = FALSE)[grepl("threat_data.rds",
+                              list.files(interim_outputs))]
 
+  threat_list <- lapply(file.path(interim_outputs, threat_files), readRDS)
+
+  all_threat_data <- do.call(rbind, threat_list)
+
+  # Check the threat data for synonyms and add tsn so we can match with
+  # species_data more accurately
+
+  threat_binomials <- get_binomial_list(all_threat_data)
+
+  # Line below takes about 15 - 20 minutes
+  
+  threat_data_synonyms <- find_synonyms(threat_binomials)
+
+  saveRDS(threat_data_synonyms, file.path(indicator_outputs,
+                                          paste(location, eco_version,
+                                                "threat_data_synonyms.rds",
+                                                sep = "_")))
+
+  # Join the tsn to the threat data
+
+  all_threat_data <- all_threat_data %>%
+    merge(threat_data_synonyms[c("tsn", "binomial",
+                                 "accepted_name")],
+          by = "binomial") %>%
+    distinct(.) %>%
+    dplyr::select(-binomial) %>%
+    rename(binomial = accepted_name)
+  
+}
+
+# Get the scheme info
+  
 threat_scheme <- read.csv(file.path(inputs, "iucn_threats\\iucn_threat_classification_scheme.csv"))
 threat_scheme$code <- as.character(threat_scheme$code)
 
@@ -2187,9 +2388,9 @@ formatted_threat_data <- all_threat_data %>%
                                 score == "High Impact: 8"|
                                 score == "High Impact: 9") %>%
                          distinct(.) %>%
-                         merge(species_data[c("binomial", "tsn", 
+                         merge(species_data[c("tsn", 
                                               "ecoregion_id")], 
-                               by = "binomial") %>%
+                               by = "tsn") %>%
                          dplyr::select(-code, - invasive) %>%
                          dplyr::select(ecoregion_id, binomial, tsn, headline,
                                        headline_name, title, included_in_hfp) %>%
@@ -2451,7 +2652,7 @@ ecoregion_values_master <- rbind(lpi_record_values,
                     ecoregion_hfp_threats,
                     ecoregion_scientific_capacity,
                     ecoregion_realms, 
-                    ecoregion_population_density, 
+                    ecoregion_populations, 
                     ecoregion_endemics) %>%
                     dplyr::select(ecoregion_id, indicator, year, 
                                   raw_indicator_value)
